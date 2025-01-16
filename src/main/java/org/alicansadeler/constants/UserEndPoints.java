0a273c1b4b98f841e68885061a07c8543cc20dcd
alicansadeler <alicansadeler@gmail.com>
2025-01-16 16:33:27 +0300

    create-login-logout path eklendi

--- src/main/java/org/alicansadeler/constants/UserEndPoints.java
+++ src/main/java/org/alicansadeler/constants/UserEndPoints.java
@@ -7,12 +7,15 @@ public class UserEndPoints {
     private static final String USER_ENDPOINT = "/user";
     private static final String CREATE_ENDPOINT = "/createWithList";
 
-    public static final String CREATE_USER = USER_ENDPOINT + CREATE_ENDPOINT;
+    public static final String CREATE_USER = USER_ENDPOINT;
     public static final String GET_USER = USER_ENDPOINT + "/{username}";
     public static final String UPDATE_USER = USER_ENDPOINT + "/{username}";
     public static final String DELETE_USER = USER_ENDPOINT + "/{username}";
+    public static final String CREATE_USERS_WITH_LIST = USER_ENDPOINT + "/createWithList";
+    public static final String USER_LOGIN = USER_ENDPOINT + "/login";
+    public static final String USER_LOGOUT = USER_ENDPOINT + "/logout";
 
-    public static RequestSpecification userAPI() {
+    public static RequestSpecification userSpec() {
         return new RequestSpecBuilder()
                 .setBaseUri(BASE_URL)
                 .setContentType(ContentType.JSON)
